409a6ddd8cbcedf71a4374cb2551379d043e1c5f
lightrao <lightrao@hotmail.com>
2025-03-23 14:37:43 +0800

    Add flutter_local_notifications setup in AppDelegate.swift for iOS notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,25 @@
 import Flutter
 import UIKit
 
+// Import the flutter_local_notifications plugin
+import flutter_local_notifications
+
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  // This is required to make any communication available in the action isolate.
+  FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+    GeneratedPluginRegistrant.register(with: registry)
+  }
+
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+  }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
